dd8d7a3a990884bcf5cae39128b56f25c8bb72d9
abe-yuya <abe-yuya@arsaga.jp>
2022-08-17 00:23:42 +0900

    feat:🎸generate json_file_computer_books

--- scrap/spiders/computer_books.py
+++ scrap/spiders/computer_books.py
@@ -13,10 +13,20 @@ class ComputerBooksSpider(CrawlSpider):
         # 最初のページを取得したら、最初の20冊の詳細ページへのリンクを辿る。取得した結果はparse_itemメソッドで処理
         Rule(LinkExtractor(restrict_xpaths='//h3[@class="heightLine-2"]/a'), callback='parse_item', follow=False),
         # 最初のページの処理が終了したら、2ページ目に遷移
-        Rule(LinkExtractor(restrict_xpaths='(//a[contains(text(), "次へ")])[1]')),
+        # Rule(LinkExtractor(restrict_xpaths='(//a[contains(text(), "次へ")])[1]')),
         )
 
 
     def parse_item(self, response):
         # 詳細ページのURLを出力
         logging.info(response.url)
+        yield {
+            'title': response.xpath('//h3[@itemprop="name"]/text()').getall(),
+            'author': response.xpath('//div[@class="infobox ml10 mt10"]/ul/li[1]/a/text()').get(),
+            'price': response.xpath('//span[@class="sale_price"]/text()').get(),
+            'publisher': response.xpath('//a[contains(@href, "publisher-key")]/text()').get(),
+            # normalize-space(xpath)：ハイフンや改行コードを取り除く
+            'size': response.xpath('normalize-space(//div[@class="infbox dotted ml10 mt05 pt05"]/ul/li/text())').get(),
+            'page': response.xpath('normalize-space(//div[@class="infbox dotted ml10 mt05 pt05"]/ul/li/text())').get(),
+            'isbn': response.xpath('//li[@itemprop="identifier"]/text()').get()
+        }
